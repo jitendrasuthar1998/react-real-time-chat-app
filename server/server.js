d5f08526e3c33eee7c479ee21b6c76e7d66c485a
jitendrasuthar <jitendrasuthar995@gmail.com>
2023-11-20 01:28:32 +0530

    wip

--- server/server.js
+++ server/server.js
@@ -17,6 +17,23 @@ const io = new Server(server, {
 
 io.on("connection", (socket)=> {
     console.log('server socket id == ', socket.id);
+
+    socket.on('joinRoom', (room)=> {
+        console.log('room name is == ', room);
+        socket.join(room);
+    })
+
+    socket.on('newMessage', ({newMessage, room})=> {
+        console.log("message is == ", newMessage);
+        console.log('room is == ', room)
+
+        io.in(room).emit('getLatestMessage', newMessage)
+    })
+})
+
+const port = 8000 || process.env.PORT 
+server.listen(port, ()=> {
+    console.log("server started on port == ", port);
 })
 
 app.get('/', (req, res)=> {
